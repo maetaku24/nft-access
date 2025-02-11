89cceae1ec700563d1bb841204c3a68578d936a9
maetaku24 <yataku24@gmail.com>
2025-02-11 16:15:42 +0900

    chore: Add various configurations for project setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,26 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        white: '#FFFFFF',
+        gray: {
+          900: '#333333'
+        },
+        green: {
+          100: '#FBF6E9',
+          200: '#E3F0AF',
+          300: '#5DB996',
+          400: '#118B50'
+        },
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
       },
     },
   },
