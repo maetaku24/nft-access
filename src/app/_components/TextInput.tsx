6a3ffbfd3db96dbe6c959886c10ffbce10289a37
maetaku24 <yataku24@gmail.com>
2025-03-18 10:57:52 +0900

    feat: shadcn/ui のコンポーネントを適用

--- src/app/_components/TextInput.tsx
+++ src/app/_components/TextInput.tsx
@@ -1,23 +1,25 @@
 import { tv, type VariantProps } from 'tailwind-variants';
 import { ComponentPropsWithRef, forwardRef } from 'react';
+import { Label } from './ui/label';
+import { Input } from './ui/input';
 
 // tv を使用して Tailwind CSS のスタイルを定義
 const textInputStyles = tv({
   slots: {
-    labelStyle: 'block mb-2 text-sm font-medium text-gray-900',
+    labelStyle: 'block mb-2 text-base font-medium text-gray-900',
     inputStyle:
-      'bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
+      'bg-gray-50 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5',
     errorStyle: 'mt-2 text-sm text-red-600 font-medium',
   },
   variants: {
     disabled: {
-      true: 'bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed',
+      true: 'bg-gray-100 border border-gray-300 text-gray-900 text-base rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 cursor-not-allowed',
     },
     error: {
       true: {
-        labelStyle: 'block mb-2 text-sm font-medium text-red-700',
+        labelStyle: 'block mb-2 text-base font-medium text-red-700',
         inputStyle:
-          'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5',
+          'bg-red-50 border border-red-500 text-red-900 placeholder-red-700 text-base rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5',
       },
     },
   },
@@ -46,10 +48,10 @@ const TextInput = forwardRef<HTMLInputElement, Props>((props, ref) => {
 
   return (
     <div>
-      <label htmlFor={id} className={labelStyle()}>
+      <Label htmlFor={id} className={labelStyle()}>
         {label}
-      </label>
-      <input
+      </Label>
+      <Input
         id={id}
         ref={ref}
         className={inputStyle()}
