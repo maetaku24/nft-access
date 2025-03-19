225bd9fa5426d94bf9ce4fd0e5e7ce0fe947a843
maetaku24 <yataku24@gmail.com>
2025-03-19 14:01:47 +0900

    feat: nftSchema を拡張

--- src/app/dashboard/_schema/nftSchema.ts
+++ src/app/dashboard/_schema/nftSchema.ts
@@ -1,18 +1,37 @@
 import { z } from 'zod';
 import { Standard, Network } from '@prisma/client';
 
-const standardEnum = z.nativeEnum(Standard);
-const networkEnum = z.nativeEnum(Network);
+const standardEnum = z.nativeEnum(Standard, {
+  errorMap: (issue, ctx) => {
+    return { message: '規格を選択してください' };
+  },
+});
+const networkEnum = z.nativeEnum(Network, {
+  errorMap: (issue, ctx) => {
+    return { message: 'ネットワークを選択してください' };
+  },
+});
 
 export const nftSchema = z
   .object({
-    collectionName: z.string().min(1, 'コレクション名を入力してください').max(20, '20文字以内で入力してください'),
+    collectionName: z
+      .string({ message: 'コレクション名を入力してください' })
+      .min(1, 'コレクション名を入力してください')
+      .max(20, '20文字以内で入力してください'),
     standard: standardEnum,
     network: networkEnum,
-    contractAddress: z.string().min(1, 'コントラクトアドレスを入力してください'),
+    contractAddress: z
+      .string({ message: 'コントラクトアドレスを入力してください' })
+      .min(1, 'コントラクトアドレスを入力してください'),
     tokenId: z.coerce.number().int().optional().nullable(),
-    minBalance: z.coerce.number().int().min(1, '1以上を入力してください'),
-    maxBalance: z.coerce.number().int().min(1, '1以上を入力してください'),
+    minBalance: z.coerce
+      .number({ message: '1以上を入力してください' })
+      .int()
+      .min(1, '1以上を入力してください'),
+    maxBalance: z.coerce
+      .number({ message: '1以上を入力してください' })
+      .int()
+      .min(1, '1以上を入力してください'),
   })
   .refine(({ minBalance, maxBalance }) => minBalance <= maxBalance, {
     message: '最大保有数は最小保有数以上にしてください',
