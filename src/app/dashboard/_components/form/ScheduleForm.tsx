c9b01ef0f77ef6819a610c3f033499c6936fcb5b
maetaku24 <yataku24@gmail.com>
2025-04-02 15:10:38 +0900

    feat: ScheduleModalの実装

---
+++ src/app/dashboard/_components/form/ScheduleForm.tsx
@@ -0,0 +1,22 @@
+'use client';
+
+import { Control } from 'react-hook-form';
+import { Event } from '@/app/dashboard/_schema/eventSchema';
+import { WeekTimePicker } from './elements/WeekTimePicker';
+import { FormCalendar } from './elements/FormCalendar';
+
+interface Props {
+  control: Control<Event>;
+}
+
+export const ScheduleForm = ({ control }: Props) => {
+  return (
+    <div>
+      <h2 className='text-2xl font-bold mb-10'>日程・参加メンバー設定</h2>
+      <div className='space-y-6 max-w-4xl'>
+        <WeekTimePicker control={control} />
+        <FormCalendar control={control} />
+      </div>
+    </div>
+  );
+};
