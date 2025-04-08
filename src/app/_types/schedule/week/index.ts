8dd8654f7f404a8a47e7f803067aeff074dedb15
maetaku24 <yataku24@gmail.com>
2025-04-08 15:16:52 +0900

    chore: ファイル構成を整理

--- src/app/_types/schedule/week/index.ts
+++ src/app/_types/schedule/week/index.ts
@@ -1,3 +1,4 @@
+// 曜日リスト
 export const WEEKDAYS = [
   '日曜日',
   '月曜日',
@@ -7,3 +8,6 @@ export const WEEKDAYS = [
   '金曜日',
   '土曜日',
 ] as const;
+
+// 曜日リストのユニオン型
+export type WeekDay = (typeof WEEKDAYS)[number];
