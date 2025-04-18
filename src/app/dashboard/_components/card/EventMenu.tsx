09af898a71ac913938e2aee8bbd278be5ac7ae47
maetaku24 <yataku24@gmail.com>
2025-04-18 23:32:47 +0900

    feat: EventCard作成

---
+++ src/app/dashboard/_components/card/EventMenu.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import { Ellipsis } from 'lucide-react';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+import { toast } from 'react-toastify';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/app/_components/ui/dropdown-menu';
+import { useSupabaseSession } from '@/app/_hooks/useSupabaseSession';
+import type { DeleteEventResponse } from '@/app/_types/event/DeleteEventResponse';
+import { deleteRequest } from '@/app/_utils/api';
+
+interface Props {
+  id: number;
+  mutate: () => void;
+}
+
+export const EventMenu: React.FC<Props> = ({ id, mutate }) => {
+  const { token } = useSupabaseSession();
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    if (confirm('削除しますか？')) {
+      try {
+        await deleteRequest<DeleteEventResponse>(
+          `/api/events/${id}`,
+          token ?? undefined
+        );
+        toast.info('イベントを削除しました');
+        mutate();
+        router.push('/dashboard');
+      } catch (error) {
+        toast.error(`イベントが削除できませんでした: ${error}`);
+      }
+    }
+  };
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Ellipsis />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className='translate-x-4 p-2'>
+        <DropdownMenuItem asChild className='py-2'>
+          <Link href={`/dashboard/events/${id}/edit`}>編集</Link>
+        </DropdownMenuItem>
+        <DropdownMenuItem className='py-2 text-red-600' onClick={handleDelete}>
+          削除
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { CreateEventCard } from './_components/card/CreateEventCard';
+import { EventCard } from './_components/card/EventCard';
 
 export default function DashboardPage() {
   return (
-    <div>
-      <div className='mt-40 flex items-start justify-center'>
+    <div className='mb-20 mt-40 flex w-full flex-col items-center space-y-4'>
+      <div className='w-full max-w-5xl px-4'>
+        <EventCard />
+      </div>
+      <div className='w-full max-w-5xl px-4'>
         <CreateEventCard />
       </div>
     </div>
