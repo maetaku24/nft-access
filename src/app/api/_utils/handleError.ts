e2d96ec00cb65f94aec97b1d4d5f9a83209edbb1
maetaku24 <yataku24@gmail.com>
2025-04-28 16:04:17 +0900

    chore: レスポンスを変更

--- src/app/api/_utils/handleError.ts
+++ src/app/api/_utils/handleError.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 // errorで渡ってくる型がわからないのでunknown型
 export const handleError = async (error: unknown) => {
   if (error instanceof Error) {
-    return NextResponse.json({ status: error.message }, { status: 400 });
+    return NextResponse.json({ message: error.message }, { status: 400 });
   }
   return NextResponse.json(
     { status: 'エラー', message: '不明なエラーが発生しました' },
