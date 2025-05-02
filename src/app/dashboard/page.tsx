1ae22391481dff6fc97b55a92dab0749c6e9ddba
maetaku24 <yataku24@gmail.com>
2025-05-03 06:57:56 +0900

    feat: 条件分岐追加

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,34 @@
-import React from 'react';
+'use client';
+
 import { CreateEventCard } from './_components/card/CreateEventCard';
 import { EventCardList } from './_components/card/EventCardList';
+import { EventCardSkeleton } from './_components/skeleton/EventCardSkeleton';
+import { useEvents } from './_hooks/useEvents';
 
 export default function DashboardPage() {
+  const { data: events, isLoading } = useEvents();
+
+  if (isLoading) {
+    return (
+      <div className='mx-auto w-full max-w-5xl'>
+        <EventCardSkeleton />
+      </div>
+    );
+  }
+
+  const hasEvents = events && events.length > 0;
+
   return (
     <div className='mb-20 mt-40 flex w-full flex-col items-center space-y-4'>
-      <div className='w-full max-w-5xl px-4'>
-        <EventCardList />
-      </div>
-      <div className='w-full max-w-5xl px-4'>
-        <CreateEventCard />
-      </div>
+      {hasEvents ? (
+        <div className='w-full max-w-5xl px-4'>
+          <EventCardList events={events} />
+        </div>
+      ) : (
+        <div className='w-full max-w-5xl px-4'>
+          <CreateEventCard />
+        </div>
+      )}
     </div>
   );
 }
