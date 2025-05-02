ad8b6d36b2a46c37178f9e50675b2d615641dd95
maetaku24 <yataku24@gmail.com>
2025-05-03 07:00:29 +0900

    feat: GETレスポンスの作成

---
+++ src/app/api/services/[userId]/[eventId]/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse, type NextRequest } from 'next/server';
+import { handleError } from '@/app/api/_utils/handleError';
+import { prisma } from '@/utils/prisma';
+
+export const GET = async (
+  request: NextRequest,
+  { params }: { params: { userId: string; eventId: string } }
+) => {
+  const eventId = parseInt(params.eventId);
+
+  try {
+    const event = await prisma.event.findFirst({
+      where: {
+        id: eventId,
+      },
+      include: {
+        profile: true,
+      },
+    });
+
+    if (!event) {
+      return NextResponse.json(
+        { message: 'イベントが存在しません' },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(
+      {
+        eventId: event.id,
+        userId: event.profile.userId,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return handleError(error);
+  }
+};
