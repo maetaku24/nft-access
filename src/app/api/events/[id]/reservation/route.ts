087392a5ba2bd271fea78f15bf8e2e0905ed327d
maetaku24 <yataku24@gmail.com>
2025-05-02 15:46:09 +0900

    feat: レスポンスの型適応

--- src/app/api/events/[id]/reservation/route.ts
+++ src/app/api/events/[id]/reservation/route.ts
@@ -5,6 +5,7 @@ import type {
   CreateReservationRequest,
   CreateReservationResponse,
 } from '@/app/_types/reservation/CreateReservation';
+import type { EventListResponse } from '@/app/_types/reservation/EventListResponse';
 import { handleError } from '@/app/api/_utils/handleError';
 import { prisma } from '@/utils/prisma';
 
@@ -28,7 +29,18 @@ export const GET = async (
         { status: 404 }
       );
     }
-    return NextResponse.json({ reservations }, { status: 200 });
+
+    const response: EventListResponse[] = reservations.map((reservation) => ({
+      id: reservation.id,
+      name: reservation.name,
+      email: reservation.email,
+      participants: reservation.participants,
+      reservationDate: reservation.reservationDate,
+      startTime: reservation.startTime,
+      endTime: reservation.endTime,
+    }));
+
+    return NextResponse.json(response, { status: 200 });
   } catch (error) {
     return handleError(error);
   }
