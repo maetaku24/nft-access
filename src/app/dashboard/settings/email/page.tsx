e845641445fdca10fd6a0ddf9e1a7130a9ddc8b6
maetaku24 <yataku24@gmail.com>
2025-05-19 21:57:59 +0900

    feat: メールアドレス変更フォームを実装

--- src/app/dashboard/settings/email/page.tsx
+++ src/app/dashboard/settings/email/page.tsx
@@ -1,25 +1,48 @@
 'use client';
 
-import { useProfileEmail } from './_hooks/useProfileEmail';
+import { useEmailUpdateForm } from './_hooks/useEmailUpdateForm';
 import TextInput from '@/app/_components/TextInput';
 import { Button } from '@/app/_components/ui/button';
 
 export default function EmailSettingsPage() {
-  const { data } = useProfileEmail();
-  if (!data) return;
+  const {
+    email,
+    handleUpdateEmail,
+    formMethods: { register, handleSubmit, formState },
+  } = useEmailUpdateForm();
+
+  if (!email) return;
+
   return (
-    <form className='flex flex-col gap-8'>
+    <form
+      onSubmit={handleSubmit(handleUpdateEmail)}
+      className='flex flex-col gap-8'
+    >
       <div>
         <h2 className='text-base font-bold'>メールアドレスの変更</h2>
       </div>
       <TextInput
-        label='メールアドレス'
-        id='email'
+        label='現在のメールアドレス'
+        id='currentEmail'
         type='email'
-        value={data.email}
+        readOnly
+        value={email}
       />
-      <Button type='button' size='lg' className='mt-5 text-base font-semibold'>
-        保存
+      <TextInput
+        label='新しいメールアドレス'
+        id='newEmail'
+        type='email'
+        {...register('email')}
+        disabled={formState.isSubmitting}
+        errorMessage={formState.errors.email?.message}
+      />
+      <Button
+        type='submit'
+        size='lg'
+        className='mt-5 text-base font-semibold'
+        disabled={formState.isSubmitting}
+      >
+        {formState.isSubmitting ? '更新中...' : '更新'}
       </Button>
     </form>
   );
