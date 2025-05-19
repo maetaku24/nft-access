402f4fe498f93f50b7dcacab2a993a6a8cda2101
maetaku24 <yataku24@gmail.com>
2025-05-19 23:11:09 +0900

    feat: usePasswordUpdateFormの実装

--- src/app/dashboard/settings/password/page.tsx
+++ src/app/dashboard/settings/password/page.tsx
@@ -1,27 +1,43 @@
 'use client';
 
+import { usePasswordUpdateForm } from './_hooks/usePasswordUpdateForm';
 import TextInput from '@/app/_components/TextInput';
 import { Button } from '@/app/_components/ui/button';
 
 export default function PasswordSettingsPage() {
+  const { register, handleSubmit, formState } = usePasswordUpdateForm();
+
   return (
-    <form className='flex flex-col gap-8'>
+    <form className='flex flex-col gap-8' onSubmit={handleSubmit}>
       <div>
         <h2 className='text-base font-bold'>パスワードの変更</h2>
       </div>
       <TextInput
         label='現在のパスワード'
-        id='current-password'
+        id='currentPassword'
         type='password'
+        {...register('currentPassword')}
+        disabled={formState.isSubmitting}
+        errorMessage={formState.errors.currentPassword?.message}
       />
-      <TextInput label='新しいパスワード' id='new-password' type='password' />
+      <TextInput
+        label='新しいパスワード'
+        id='newPassword'
+        type='password'
+        {...register('newPassword')}
+        disabled={formState.isSubmitting}
+        errorMessage={formState.errors.newPassword?.message}
+      />
       <TextInput
         label='新しいパスワード（確認）'
-        id='confirm-password'
+        id='passwordConfirmation'
         type='password'
+        {...register('passwordConfirmation')}
+        disabled={formState.isSubmitting}
+        errorMessage={formState.errors.passwordConfirmation?.message}
       />
-      <Button type='button' size='lg' className='mt-5 text-base font-semibold'>
-        保存
+      <Button type='submit' size='lg' className='mt-5 text-base font-semibold'>
+        {formState.isSubmitting ? '更新中...' : '更新'}
       </Button>
     </form>
   );
